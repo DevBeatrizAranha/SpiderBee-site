328a39d754e8ad75646f79a616094d6df6501a93
DevBeatrizAranha <bmja.dev@gmail.com>
2025-10-03 16:38:10 -0300

    site em construção

--- svelte@latest/src/routes/+page.svelte
+++ svelte@latest/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div style="display:flex; justify-content: center; align-items: center;">
+  <video autoplay muted loop playsinline style="width: 40%;">
+    <source src="/assets/timelapse2.mp4" type="video/mp4" />
+  </video>
+</div>
+<div style="display:flex; justify-content: center; align-items: center;">
+    <h1> Preparando Site</h1>
+</div>
+<footer  style="display:flex;justify-content: center;align-content: flex-end;align-items: flex-end;">
+    <h6>Todos os direitos reservados. SpiderBee Game Studio © 2025</h6>
+</footer>
